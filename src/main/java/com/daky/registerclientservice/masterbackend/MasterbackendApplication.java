a7d3f52199be6586251c616b3baebbd080b240ed
dmi3uk <Dmi3uk@yandex.ru>
2023-02-08 17:03:37 +0200

    init

---
+++ src/main/java/com/daky/registerclientservice/masterbackend/MasterbackendApplication.java
@@ -0,0 +1,13 @@
+package com.daky.registerclientservice.masterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daky/registerclientservice/masterbackend/MasterbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daky.registerclientservice.masterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
